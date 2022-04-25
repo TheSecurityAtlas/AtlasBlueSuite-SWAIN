b99be02a946dd57e11cf72882c8c21d365fa868c
TheSecurityAtlas <info@thesecurityatlas.com>
2022-04-25 14:06:38 -0400

    Update

--- SWAIN-PurgeTool.py
+++ SWAIN-PurgeTool.py
@@ -8,19 +8,15 @@ import os
 # this will change the default shell to use powershell instead of cmd.exe or /bin/sh
 os.environ["COMSPEC"] = "powershell"
 
-
-
-
 def CSCT():
-    CSCT_Connect = input("Please provide your domain email address ")
+    CSCT_Connect = input("Please provide your domain email address: ")
     CSCT_Name = input("What is the name of your Content Search? ")
-    CSCT_Description = input("Please give this Content Search a concise description ")
-    CSCT_Location = input("Which mailboxes would you like to search? 'Please use (All) to search all mailboxes' :")
-    CSCT_Query = input("What are the search parameters? (Please consult the 'Search Parameters.txt' documentations) :")
+    CSCT_Description = input("Please give this Content Search a concise description: ")
+    CSCT_Location = input("Which mailboxes would you like to search? 'Please use (All) to search all mailboxes': ")
+    CSCT_Query = input("What are the search parameters? (Please consult the 'Search Parameters.txt' documentations) : ")
         
     sp.run(f"Connect-IPPSSession -UserPrincipalName {CSCT_Connect} ; New-ComplianceSearch -Name {CSCT_Name!r} -AllowNotFoundExchangeLocationsEnabled $true -ContentMatchQuery {CSCT_Query!r} -Description {CSCT_Description!r} -ExchangeLocation {CSCT_Location} ; Start-ComplianceSearch -Identity {CSCT_Name}", shell=True)
-    print(f"{CSCT_Name} has been started")
-
+    print(f"{CSCT_Name} has been started, Search will be complete soon, please wait 2-5 minitues")
 
 def SWAIN():
     Email = input("Please provide your domain email address ")
@@ -33,15 +29,15 @@ def SWAIN():
         print("Please enter [SoftDelete] or [HardDelete]")
 
 
-Content_Search = input("Have you started a Content Search yet? (Y) or (N) ")
+#Content_Search = input("Have you started a Content Search yet? (Y) or (N) ")
 
-if Content_Search == "Y":
-    SWAIN()
-elif Content_Search == "N":
-    CSCT()
-    SWAIN()
-
-
+#if Content_Search == "Y":
+#    SWAIN()
+#elif Content_Search == "N":
+#    CSCT()
+#    SWAIN()
 
 
+
+print("Search Query: Quick Reference Guide \nAttachmentNames  ")
 
