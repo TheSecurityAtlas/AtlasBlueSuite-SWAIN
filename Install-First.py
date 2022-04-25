f416759dbb389028283e4cff04907fd9628ecabe
TheSecurityAtlas <info@thesecurityatlas.com>
2022-04-25 00:51:34 -0400

    Update

--- Install-First.py
+++ Install-First.py
@@ -2,8 +2,10 @@
 # Install these dependencies first
 
 import subprocess as sp
+import os
 
-Dependencies = "Install-Module -Name ExchangeOnlineManagement ; Set-ExecutionPolicy RemoteSigned"
+os.environ["COMSPEC"] = "powershell"
+Dependencies = "Install-Module -Name ExchangeOnlineManagement ; Set-PSRepository -Name PSGallery -InstallationPolicy Trusted"
 
 
 sp.run(Dependencies, shell=True)
