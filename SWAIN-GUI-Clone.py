f93c6f429a4e5c0ca630b1fd8379790b13fdc704
TheSecurityAtlas <info@thesecurityatlas.com>
2022-04-27 15:11:40 -0400

    commit

--- SWAIN-GUI-Clone.py
+++ SWAIN-GUI-Clone.py
@@ -2,6 +2,7 @@
 # v1.2
 
 # importing modules
+import time
 import subprocess as sp
 import os
 
@@ -12,6 +13,8 @@ def Query_info():
     print("Search Query Quick Reference Guide: \nFor more about Search Query Syntax please visit Syntax_Guide.txt\n\nFrom\n  from:<email@domain.com> -- can also be just domain: from:<domain.com>\n\nRecipients (includes all recipients in 'To' 'cc' 'bcc')\n  recipients:<email@domain.com> can also be just domain: recipients:<domain.com>\n\nSubject (the query doesn't return only those messages that have an exact match.)\n subject:<insert subject here> \n\nReceived \nThe date that an email message was received by a recipient \n  received:2021-04-15 -- date is a single day\n   received>=2021-01-01 AND received<=2021-03-31 -- date is a range\n\n\nExample query:\n from:hotsauce@aol.com AND subject:Budget Reports AND received:2022-02-10\n\n")
 
 def CSCT():
+    
+
     print("\n\nWelcome to the CSCT: Content Search Creation Tool")
     CSCT_Connect = input("Please provide your domain email address: ")
     CSCT_Name = input("What is the name of your Content Search? ")
@@ -19,9 +22,8 @@ def CSCT():
     CSCT_Location = input("Which mailboxes would you like to search? 'Please use (All) to search all mailboxes' : ")
     Query_info()
     CSCT_Query = input("What are the search parameters? ")
-        
     sp.run(f"Connect-IPPSSession -UserPrincipalName {CSCT_Connect} ; New-ComplianceSearch -Name {CSCT_Name!r} -AllowNotFoundExchangeLocationsEnabled $true -ContentMatchQuery {CSCT_Query!r} -Description {CSCT_Description!r} -ExchangeLocation {CSCT_Location} ; Start-ComplianceSearch -Identity {CSCT_Name}", shell=True)
-    print(f"{CSCT_Name} has been started, Search will be complete soon, please wait 2-5 minitues: Happy Hunting")
+    print(f"{CSCT_Name} has been started, Search will be complete soon, Happy Hunting!")
 
 def SWAIN():
     print("\n\nWelcome to SWAIN: Email Purge Tool")
@@ -40,5 +42,6 @@ if Content_Search == "Y":
     SWAIN()
 elif Content_Search == "N":
     CSCT()
+    print("Please wait for 120 seconds...")
+    time.sleep(120)
     SWAIN()
-
