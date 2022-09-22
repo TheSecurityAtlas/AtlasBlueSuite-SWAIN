dc4b7f1d4b90f414f546ddfc7f3251851a34c5ed
Andrew Heishman <info@thesecurityatlas.com>
2022-09-22 12:15:57 -0400

    Removed description and added other options

--- SWAIN-Main.py
+++ SWAIN-Main.py
@@ -6,9 +6,9 @@ import time
 import subprocess as sp
 import os
 
-# this will change the default shell to use powershell instead of cmd.exe or /bin/sh
+# this will change the default shell to use powershell instead of cmd
 os.environ["COMSPEC"] = "powershell"
-progress = list(range(0, 157))
+progress = list(range(0, 2))
 l = len(progress)
 
 
@@ -22,6 +22,7 @@ def SWAIN_bar (iteration, total, prefix = '', suffix = '', decimals = 1, length 
     print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
     if iteration == total: 
         print("\n")
+        
 
 def ProgressBar():
     SWAIN_bar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
@@ -34,13 +35,11 @@ def CSCT():
     print("\n\nWelcome to the CSCT: Content Search Creation Tool")
     CSCT_Connect = input("Please provide your domain email address: ")
     CSCT_Name = input("What is the name of your Content Search? ")
-    CSCT_Description = input("Please give this Content Search a concise description: ")
     CSCT_Location = input("Which mailboxes would you like to search? 'Please use 'All' to search all mailboxes' : ")
     Query_info()
     CSCT_Query = input("What are the search parameters? ")
-    sp.run(f"Connect-IPPSSession -UserPrincipalName {CSCT_Connect} ; New-ComplianceSearch -Name {CSCT_Name!r} -AllowNotFoundExchangeLocationsEnabled $true -ContentMatchQuery {CSCT_Query!r} -Description {CSCT_Description!r} -ExchangeLocation {CSCT_Location} ; Start-ComplianceSearch -Identity {CSCT_Name}", shell=True)
+    sp.run(f"Connect-IPPSSession -UserPrincipalName {CSCT_Connect} ; New-ComplianceSearch -Name {CSCT_Name!r} -AllowNotFoundExchangeLocationsEnabled $true -ContentMatchQuery {CSCT_Query!r} -Description Search started from SWAIN by {CSCT_Name} -ExchangeLocation {CSCT_Location} ; Start-ComplianceSearch -Identity {CSCT_Name}", shell=True)
     print(f"{CSCT_Name} has been started, Search will be complete soon, Happy Hunting!")
-
 def SWAIN():
     print("\n\nWelcome to SWAIN: Email Purge Tool")
     Email = input("Please provide your domain email address ")
@@ -49,21 +48,19 @@ def SWAIN():
     Email_Purge = f"Connect-IPPSSession -UserPrincipalName {Email} ; New-ComplianceSearchAction -SearchName {Search_Name!r} -purge -PurgeType {Purge_Type}"
     if Purge_Type == "SoftDelete" or Purge_Type == "HardDelete":
         sp.run(Email_Purge, shell=True)
-        print(f"\n\n{Search_Name} has been started. \nIf you would like to see the status of your purge -- please use Get-ComplianceSearchAction <insert name here> (remember to include _purge) ")
         input("Please press enter to close this application")
     else:
         print("Please enter 'SoftDelete' or 'HardDelete'")
 
-Main_Query = input("  _______          __     _____ _   _ \n / ____\ \        / /\   |_   _| \ | |\n| (___  \ \  /\  / /  \    | | |  \| |\n \___ \  \ \/  \/ / /\ \   | | | . ` |\n ____) |  \  /\  / ____ \ _| |_| |\  |\n|_____/    \/  \/_/    \_\_____|_| \_|\n\nThis tool will start the purging process by having you create a 'Content Search'. Or maybe you'd like to do something different?\n\n'1': Start the search for emails\n'2': Purge emails using the search created in option 1\n'3': Block an email address\n\nWhat would you like to do?:")
+while True:
+    Main_Query = input("  _______          __     _____ _   _ \n / ____\ \        / /\   |_   _| \ | |\n| (___  \ \  /\  / /  \    | | |  \| |\n \___ \  \ \/  \/ / /\ \   | | | . ` |\n ____) |  \  /\  / ____ \ _| |_| |\  |\n|_____/    \/  \/_/    \_\_____|_| \_|\n\nThis tool will start the purging process by having you create a 'Content Search'. Or maybe you'd like to do something different?\n\n'1': Start the search for emails\n'2': Purge emails using the search created in option 1\n'3': Block an email address\n\n'S': Syntax guide for Content Search\n'E': Exit\n'firstrun': Use this option when you have never used SWAIN on a computer before\n\nWhat would you like to do? ")
+    if Main_Query == "1":
+        CSCT()
+        print("Content search should take about 2 - 5 minutes")
+    elif Main_Query == "2":
+        SWAIN()
+        print("Purge started!\nIf you would like to see the status of your purge -- please use 'Get-ComplianceSearchAction <insert name here>_purge' (remember to include '_purge')")
+    elif Main_Query == "3":
+        print("This is a test string")
+        ProgressBar()
 
-if Main_Query == "2":
-    SWAIN()
-elif Main_Query == "1":
-    CSCT()
-    print("Content search should take about 2 - 5 minutes")
-    time.sleep(300)
-    SWAIN()
-elif Main_Query == "3":
-    ProgressBar()
-    print("This is a test string")
-    
